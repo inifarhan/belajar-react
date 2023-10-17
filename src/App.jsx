e7ddc4f57dec215521d49f5f0c408dbb20096537
inifarhan <farhangunawan619@gmail.com>
2023-10-17 19:40:03 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+      <Header />
+    </div>
+  )
+}
+
+export default App
